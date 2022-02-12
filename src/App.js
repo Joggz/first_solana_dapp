7a340580886ba216278096e3566b82c2c2f09063
laoye_jnr <idrisaboladeo@gmail.com>
2022-02-12 21:23:51 +0100

    :construction_worker: show connect button dependent on if app is connected to phantom

--- src/App.js
+++ src/App.js
@@ -8,21 +8,31 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
-  const checkIfWalletIsConnected = async () => {
-    const connectWallet = async () => {};
 
-    /*
-     * We want to render this UI when the user hasn't connected
-     * their wallet to our app yet.
-     */
-    const renderNotConnectedContainer = () => (
-      <button
-        className="cta-button connect-wallet-button"
-        onClick={connectWallet}
-      >
-        Connect to Wallet
-      </button>
-    );
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with Public Key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  /*
+   * We want to render this UI when the user hasn't connected
+   * their wallet to our app yet.
+   */
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
 
@@ -47,6 +57,9 @@ const App = () => {
    * When our component first mounts, let's check to see if we have a connected
    * Phantom Wallet
    */
+  // connect wallet on mount
+  // shifts it to the button click event
+
   useEffect(() => {
     const onLoad = async () => {
       await checkIfWalletIsConnected();
@@ -62,6 +75,7 @@ const App = () => {
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
